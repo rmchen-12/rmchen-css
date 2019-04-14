f8f129a83ce3efe1bafd844f5d00da5d74dc8a16
rmchen-12 <rmchen1993@163.com>
2019-04-14 12:29:43 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# rmchen-css
+当在 next 中使用 antd-mobile 时想使用 css-modules，在 next-config.js 直接开启会出错，因为@zeit/css 默认不会对 antd-mobile 处理
+
+使用
+
+```
+ withCss({
+    cssModules: true,
+    antdMobileLoaderOptions: {
+      cssModules: false
+    }
+ })
+```
---
+++ index.js
@@ -0,0 +1,57 @@
+const cssLoaderConfig = require("./css-loader-config");
+
+module.exports = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      if (!options.defaultLoaders) {
+        throw new Error(
+          "This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade"
+        );
+      }
+
+      const { dev, isServer } = options;
+      const {
+        cssModules,
+        cssLoaderOptions,
+        antdMobileLoaderOptions,
+        postcssLoaderOptions
+      } = nextConfig;
+
+      options.defaultLoaders.css = cssLoaderConfig(config, {
+        extensions: ["css"],
+        cssModules,
+        cssLoaderOptions,
+        postcssLoaderOptions,
+        dev,
+        isServer
+      });
+
+      config.module.rules.push({
+        test: /\.css$/,
+        exclude: [/node_modules/],
+        use: options.defaultLoaders.css
+      });
+
+      options.defaultLoaders.css = cssLoaderConfig(config, {
+        extensions: ["css"],
+        cssModules: antdMobileLoaderOptions.cssModules,
+        cssLoaderOptions,
+        postcssLoaderOptions,
+        dev,
+        isServer
+      });
+
+      config.module.rules.push({
+        test: /\.css$/,
+        include: [/node_modules/],
+        use: options.defaultLoaders.css
+      });
+
+      if (typeof nextConfig.webpack === "function") {
+        return nextConfig.webpack(config, options);
+      }
+
+      return config;
+    }
+  });
+};
